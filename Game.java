ad957b66246f416ec863d179c70a4f324d5afa08
unknown <2000040811@804CTE-J161YY1.fcs.org>
2014-05-08 11:10:05 -0400

    Game Finished

---
+++ Game.java
@@ -0,0 +1,9 @@
+import java.game.bears.bearnoses.bearnosesimulator.game[];
+
+public class Game
+{
+  public static void main(String args[])
+  {
+    System.out.println("Bearnose Collector Simulator 2014");
+  }
+}
